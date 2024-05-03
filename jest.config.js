7710343e14ce4052f6efb583360a522b28a00f1b
OmiDev <omarblanco54@gmail.com>
2024-05-02 22:07:33 -0400

    add and configure unit testing

---
+++ jest.config.js
@@ -0,0 +1,47 @@
+// module.exports = {
+// 	testEnvironment: "jsdom",
+// 	setupFilesAfterEnv: ["<rootDir>/tests/setupTests.ts"],
+// 	testMatch: ["<rootDir>/tests/**/*.(spec).(ts|tsx)"],
+// 	testPathIgnorePatterns: ["<rootDir>/tests/e2e/"],
+// 	transform: {
+// 		"^.+\\.(js|jsx|ts|tsx)$": [
+// 			"@swc/jest",
+// 			{
+// 				sourceMaps: true,
+// 				jsc: {
+// 					parser: {
+// 						syntax: "typescript",
+// 						tsx: true,
+// 					},
+// 					transform: {
+// 						react: {
+// 							runtime: "automatic",
+// 						},
+// 					},
+// 				},
+// 			},
+// 		],
+// 	},
+	
+// };
+
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/tests/setupTests.ts"],
+  testEnvironment: "jsdom",
+  moduleNameMapper: {
+		"\\.(css|less|scss|sass)$": "identity-obj-proxy",
+		"\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+			"jest-transform-stub",
+	},
+};
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
